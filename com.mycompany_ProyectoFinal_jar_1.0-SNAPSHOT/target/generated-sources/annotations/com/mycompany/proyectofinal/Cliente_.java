b962d1452368c7476a3b0f230829a2ab554b4c10
laraduarte2 <duartelara202@gmail.com>
2026-03-30 14:30:32 -0300

    remove target from tracking

--- com.mycompany_ProyectoFinal_jar_1.0-SNAPSHOT/target/generated-sources/annotations/com/mycompany/proyectofinal/Cliente_.java
+++ com.mycompany_ProyectoFinal_jar_1.0-SNAPSHOT/target/generated-sources/annotations/com/mycompany/proyectofinal/Cliente_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2026-03-20T17:57:34", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2026-03-30T14:29:36", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
--- com.mycompany_ProyectoFinal_jar_1.0-SNAPSHOT/target/generated-sources/annotations/com/mycompany/proyectofinal/Servicio_.java
+++ com.mycompany_ProyectoFinal_jar_1.0-SNAPSHOT/target/generated-sources/annotations/com/mycompany/proyectofinal/Servicio_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2026-03-20T17:57:34", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2026-03-30T14:29:36", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Servicio.class)
 public class Servicio_ { 
 
